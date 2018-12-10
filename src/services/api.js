63491ea5a7202b91e4b1f27c4792877079602550
zwOvO <974226358@qq.com>
2018-12-10 21:18:33 +0800

    Jenkins测试|2018年12月10日21:18:25

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import request from '@/utils/request';
 
 // const apiUrl = "http://192.168.31.235:8080/oa-web";
 // const apiUrl = "http://192.168.43.64:8080/oa-web";
-
+//
 const apiUrl = "https://www.zwovo.club/oa-web";
 
 export async function queryProjectNotice() {
